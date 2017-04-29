2be36fc519a9bd2c81f28760a0ad008f2612eb97
Ajki <flix.ajki@gmail.com>
2017-04-29 17:11:40 +0200

    Update plexupdatenewfolders.sh
    Complete script overhaul:
    1. Instead of checking for new/modifies folders the script will check for new/modified files ( since some clouds do not modify folder dates when content in them changes ) 
    2. Building the array with unique folder names before running Plex Scan, since that one takes long time and uploads during those may be missed.
    
    Checking for new files in Movie & TV Shows folder and building the unique folder list

--- plex/plexupdatenewfolders.sh
+++ plex/plexupdatenewfolders.sh
@@ -14,7 +14,9 @@ MOVIESECTION=2
 TVLIBRARY="/mnt/cloud/series/"
 TVSECTION=1
 LOGFILE="/home/plex/logs/plexrefreshnew.cron.log"
+FOLDERLISTFILE="/home/plex/.cache/folderlistfile"
 LASTRUNFILE="/home/plex/.cache/lastrunfile"
+
 
 export LD_LIBRARY_PATH=/usr/lib/plexmediaserver
 export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR=/var/lib/plexmediaserver/Library/Application\ Support
@@ -22,21 +24,54 @@ export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR=/var/lib/plexmediaserver/Librar
 if [[ ! -f "$LASTRUNFILE" ]]; then
     touch $LASTRUNFILE
 fi
-echo "$(date "+%d.%m.%Y %T") PLEX SCAN FOLDERS MODIFIED AFTER: $(date -r $LASTRUNFILE)"
-#UPDATE MOVIES
+echo "$(date "+%d.%m.%Y %T") PLEX SCAN FOR NEW/MODIFIED FILES AFTER: $(date -r $LASTRUNFILE)"
+
+if [[ -f "$FOLDERLISTFILE" ]]; then
+    echo "Removing previous folder list"
+    rm $FOLDERLISTFILE
+fi
+
 start=$(date +'%s')
 startmovies=$(date +'%s')
-echo "Scaning movies at: $MOVIELIBRARY"
-find "$MOVIELIBRARY" -mindepth 1 -type d -cnewer $LASTRUNFILE -exec \
-/usr/lib/plexmediaserver/Plex\ Media\ Scanner --scan --refresh --section "$MOVIESECTION" --directory {} \; | tee -a "$LOGFILE"
-echo "$(date "+%d.%m.%Y %T") Movies scanned in $(($(date +'%s') - $startmovies)) seconds" | tee -a "$LOGFILE"
-echo " " | tee -a "$LOGFILE"
-#UPDATE TV SHOWS
+echo "Scaning for new files: $MOVIELIBRARY"
+find "$MOVIELIBRARY" -mindepth 1 -type f -cnewer $LASTRUNFILE |
+while read mfile; do
+        echo "$(date "+%d.%m.%Y %T") New file detected: $mfile" | tee -a "$LOGFILE"
+        MFOLDER=$(dirname "${mfile}")
+        echo "$MFOLDER" | tee -a "$FOLDERLISTFILE"
+done
+echo "$(date "+%d.%m.%Y %T") Movie files scanned in $(($(date +'%s') - $startmovies)) seconds" | tee -a "$LOGFILE"
+
 startseries=$(date +'%s')
-echo "Scaning TV at: $TVLIBRARY"
-find "$TVLIBRARY" -mindepth 2 -type d -cnewer $LASTRUNFILE -exec \
-/usr/lib/plexmediaserver/Plex\ Media\ Scanner --scan --refresh --section "$TVSECTION" --directory {} \; | tee -a "$LOGFILE"
-echo "$(date "+%d.%m.%Y %T") Series scanned in $(($(date +'%s') - $startseries)) seconds" | tee -a "$LOGFILE"
-echo "$(date "+%d.%m.%Y %T") Scan completed in $(($(date +'%s') - $start)) seconds" | tee -a "$LOGFILE"
+echo "Scaning for new files: $TVLIBRARY"
+find "$TVLIBRARY" -mindepth 2 -type f -cnewer $LASTRUNFILE |
+while read tvfile; do
+        echo "$(date "+%d.%m.%Y %T") New file detected: $tvfile" | tee -a "$LOGFILE"
+        TVFOLDER=$(dirname "${tvfile}")
+        echo "$TVFOLDER" | tee -a "$FOLDERLISTFILE"
+done
+echo "$(date "+%d.%m.%Y %T") TV folders scanned in $(($(date +'%s') - $startseries)) seconds" | tee -a "$LOGFILE"
+
+echo "$(date "+%d.%m.%Y %T") Move & TV folders scanned in $(($(date +'%s') - $start)) seconds" | tee -a "$LOGFILE"
+echo "$(date "+%d.%m.%Y %T") Setting lastrun for next folder scans" | tee -a "$LOGFILE"
 touch $LASTRUNFILE
+echo "$(date "+%d.%m.%Y %T") Remove duplicates" | tee -a "$LOGFILE"
+sort $FOLDERLISTFILE | uniq | tee $FOLDERLISTFILE
+
+startplexscan=$(date +'%s')
+echo "$(date "+%d.%m.%Y %T") Plex scan started" | tee -a "$LOGFILE"
+readarray -t FOLDERS < "$FOLDERLISTFILE"
+for FOLDER in "${FOLDERS[@]}"
+do
+    if [[  $FOLDER == "$MOVIELIBRARY"* ]]; then
+        echo "$(date "+%d.%m.%Y %T") Plex scan movie folder:: $FOLDER" | tee -a "$LOGFILE"
+        $LD_LIBRARY_PATH/Plex\ Media\ Scanner --scan --refresh --section "$MOVIESECTION" --directory "$FOLDER" | tee -a "$LOGFILE"
+    elif [[  $FOLDER == "$TVLIBRARY"* ]]; then
+        echo "$(date "+%d.%m.%Y %T") Plex scan TV folder: $FOLDER" | tee -a "$LOGFILE"
+        $LD_LIBRARY_PATH/usr/lib/plexmediaserver/Plex\ Media\ Scanner --scan --refresh --section "$TVSECTION" --directory "$FOLDER" | tee -a "$LOGFILE"
+    fi
+done
+echo "$(date "+%d.%m.%Y %T") Plex scan finished in $(($(date +'%s') - $startplexscan)) seconds" | tee -a "$LOGFILE"
+
+echo "$(date "+%d.%m.%Y %T") Scan completed in $(($(date +'%s') - $start)) seconds" | tee -a "$LOGFILE"
 exit
