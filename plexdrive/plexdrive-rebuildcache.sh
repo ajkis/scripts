f5d33d5c378653efa0499f75e06b3cd9d9e7d511
Ajki <flix.ajki@gmail.com>
2017-05-28 09:05:47 +0200

    Update plexdrive-rebuildcache.sh

--- plexdrive/plexdrive-rebuildcache.sh
+++ plexdrive/plexdrive-rebuildcache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## plexdrive-rebuildcache.sh (chmod a+x plexdrive-rebuildcache.sh)
-## The function of this script is rebuild cache in memory and then move it to default location.
+## The function of this script is rebuild cache with paralel process in memory and then move it to default location.
 ## NOTE: This will speed up process significantly as building full cache on hard disk may take hours.
 ##
 ## More scripts at: https://github.com/ajkis/scripts
@@ -12,25 +12,42 @@ PLEXDRIVEEXEC=/usr/bin/plexdrive # Set path to plexdrive executable
 PLEXDRIVEMNT=/mnt/plexdrive # Set path to plexdrive mount
 PLEXDRIVECONF=~/.plexdrive # Set location of your plexdrive config or leave default
 PLEXDRIVEMOUNTCMD="/home/plex/scripts/mountplexdrive" # Set path to your plexdrive mount script. If you use systemd replace it with "sudo systemctl plexdrive.service start"
+PLEXDRIVETMPMNT=/mnt/tmp #Make sure you have read/write permissions in /mnt
 
-# Specify list of commands to stop apps & unmount drives before proceeding with cache rebuild
-## example: sudo service plexmediaserver stop
-STOPCMDS=(
-     'fusermount -uz /mnt/plexdrive'
-)
-# Specify list of commands to mount and start apps once cache is rebuilt
-## If you mount plexdrive with systemd replace it with: sudo systemctl plexdrive.service star
-## example: sudo service plexmediaserver start
-STARTCMDS=(
-     'echo "/home/plex/scripts/mountplexdrive"'
-)
+echo "$(date "+%d.%m.%Y %T") plexdrive rebuild cache started" | tee -a $LOGFILE
 
 
-if [[ ! -f $PLEXDRIVECONF/config.json ]]; then
-    echo "EXIT: plexdrive config missing at path $PLEXDRIVECONF"
-    echo "NOTE: plexdrive must be configured, type: plexdrive /mnt/plexdrive and follow setup"
-    exit
+if [[ ! -d $PLEXDRIVETMPMNT ]]; then
+    mkdir $PLEXDRIVETMPMNT
 fi
+
+## REMOVE OLD FILES IN CASE SCRIPT WAS ABORTED
+rm -f /dev/shm/cache* >/dev/null
+rm -f /dev/shm/config.json >/dev/null
+rm -f /dev/shm/token.json  >/dev/null
+rm -f /dev/shm/plexdrivetmp.log >/dev/null
+rm -rf /dev/shm/chunks >/dev/null
+
+echo "Copying plex drive config to $PLEXDRIVETMPMNT" | tee -a $LOGFILE
+cp $PLEXDRIVECONF/config.json /dev/shm | tee -a $LOGFILE
+cp $PLEXDRIVECONF/token.json /dev/shm | tee -a $LOGFILE
+
+echo "Mounting TMP plexdrive and waiting for cache to be rebuilt" | tee -a $LOGFILE
+cachebuildstart=$(date +'%s')
+$PLEXDRIVEEXEC $PLEXDRIVETMPMNT --config=/dev/shm --temp=/dev/shm -v 2 &>>/dev/shm/plexdrivetmp.log &
+while > /dev/null
+do
+  finished=$(grep "First cache build process finished!" /dev/shm/plexdrivetmp.log)
+  if [[ ! -z $finished ]]; then
+    echo "First cache build process finished in $(($(date +'%s') - $cachebuildstart)) seconds" | tee -a $LOGFILE
+    echo "$(grep "Processed" /dev/shm/plexdrivetmp.log | tail -1)" | tee -a $LOGFILE
+    fusermount -uz $PLEXDRIVETMPMNT
+    break
+  fi
+    echo "$(grep -i "${PLEXDRIVEEXEC:1}" /dev/shm/plexdrivetmp.log | tail -1)"
+    sleep 1
+done
+
 echo "///////////////////////////////////////////////////////////////////////////////////////////////"
 echo "Your current cache will be deleted, rebuilt in memory (/dev/shm) and moved to default location"
 echo "plexdrive conf path = $PLEXDRIVECONF"
@@ -44,52 +61,39 @@ if [[ $response != y ]]; then
 fi
 case "$response" in
     [yY][eE][sS]|[yY])
-        echo "$(date "+%d.%m.%Y %T") plexdrive rebuild cache started" | tee -a $LOGFILE
-        echo "Unmounting drive and stopping apps" | tee -a $LOGFILE
-        for stopcmd in "${STOPCMDS[@]}"
-        do
-            $stopcmd | tee -a $LOGFILE
-        done
-        echo "Copying plex drive config to /dev/shm" | tee -a $LOGFILE
-        cp $PLEXDRIVECONF/config.json /dev/shm/ | tee -a $LOGFILE
-        cp $PLEXDRIVECONF/token.json /dev/shm/ | tee -a $LOGFILE
-        if [[ ! -f /dev/shm/cache ]]; then
-            echo "Deleting existing cache file" | tee -a $LOGFILE
-            rm /dev/shm/cache* | tee -a $LOGFILE
-        fi
-        echo "Mounting plexdrive and waiting for cache to be rebuilt" | tee -a $LOGFILE
-        cachebuildstart=$(date +'%s')
-        $PLEXDRIVEEXEC $PLEXDRIVEMNT --config=/dev/shm -v 2 &>>/dev/shm/plexdrive-$TIMESTAMP.log &
-        while > /dev/null
-        do
-          finished=$(grep "First cache build process finished!" /dev/shm/plexdrive-$TIMESTAMP.log)
-          if [[ ! -z $finished ]]; then
-            echo "First cache build process finished in $(($(date +'%s') - $cachebuildstart)) seconds" | tee -a $LOGFILE
-            echo "$(grep "Processed" /dev/shm/plexdrive-$TIMESTAMP.log | tail -1)" | tee -a $LOGFILE
-            break
-          fi
-            echo "$(grep -i "${PLEXDRIVEEXEC:1}" /dev/shm/plexdrive-$TIMESTAMP.log | tail -1)"
-            sleep 1
-        done
-        echo "Unmounting plexdrive" | tee -a $LOGFILE
-        fusermount -uz $PLEXDRIVEMNT | tee -a $LOGFILE
-        echo "/dev/shm cleanup" | tee -a $LOGFILE
-        rm /dev/shm/config.json | tee -a $LOGFILE
-        rm /dev/shm/token.json  | tee -a $LOGFILE
-        rm /dev/shm/plexdrive-$TIMESTAMP.log | tee -a $LOGFILE
-        echo "Moving plexdrive cache back to $PLEXDRIVECONF" | tee -a $LOGFILE
-        mv /dev/shm/cache* $PLEXDRIVECONF | tee -a $LOGFILE
+    ### LIST OF COMMAND TO RUN BEFORE REMOUNTING PLEXDRIVE
+    ## eg: sudo service plexmediaserver stop
+    echo "Stopping all services" | tee -a $LOGFILE
+    fusermount -uz /mnt/plexdrivecrypt
+    fusermount -uz $PLEXDRIVEMNT
+    sudo systemctl stop radarr.service
+    sudo systemctl stop sonarr.service
+    rm -rf /home/plex/.plexdrive/temp/chunks
+    rm -rf /home/plex/.plexdrive/cache
+    mv /home/plex/scripts/cron/local2cloud.cron /home/plex/scripts/cron/local2cloud.cron2
+    mv /home/plex/scripts/cron/cloudmount.cron /home/plex/scripts/cron/cloudmount.cron2
 
-        echo "Mounting plexdrive and starting all services" | tee -a $LOGFILE
-        for startmd in "${STARTCMDS[@]}"
-        do
-            $startcmd | tee -a $LOGFILE
-        done
+    ### MOVE NEW CACHE FILE
+    echo "Moving new plexdrive cache to $PLEXDRIVECONF" | tee -a $LOGFILE
+    mv /dev/shm/cache* $PLEXDRIVECONF | tee -a $LOGFILE
+
+    ### CLEANUP
+    echo "/dev/shm cleanup" | tee -a $LOGFILE
+    rm /dev/shm/config.json | tee -a $LOGFILE
+    rm /dev/shm/token.json  | tee -a $LOGFILE
+    rm /dev/shm/plexdrivetmp.log | tee -a $LOGFILE
+    rm -rf /dev/shm/chunks | tee -a $LOGFILE
+
+    ###  LIST OF COMMANDS TO PREFORM ONCE DRIVE IS REMOUNTED
+    ## Start srvices stopped before remount
+    echo "Mounting plexdrive and starting all services" | tee -a $LOGFILE
+    /home/plex/scripts/mountplexdrive
+    echo "$(date "+%d.%m.%Y %T") plexdrive rebuild cache finished" | tee -a $LOGFILE
         ;;
     *)
         echo "Aborted"
         exit
         ;;
 esac
-        echo "$(date "+%d.%m.%Y %T") plexdrive rebuild cache finished" | tee -a $LOGFILE
+
 exit
