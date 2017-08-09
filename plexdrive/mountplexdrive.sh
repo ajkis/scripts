eae3a545bc7e01f748dc02e9360a19fe313a6e9f
Ajki <flix.ajki@gmail.com>
2017-08-09 06:52:10 +0200

    Update mountplexdrive.sh

--- plexdrive/mountplexdrive.sh
+++ plexdrive/mountplexdrive.sh
@@ -25,37 +25,17 @@ if mountpoint -q $MPOINT ; then
     echo "$MPOINT already mounted"
 else
     echo "Mounting $MPOINT"
+    ## Adjust chunk-check/load threads to match maximum concurrent streams
+    ## Do not use losd-ahead bigger then 10.
+    ## Keep in mind that 1080p stream will need 20Mbit while some scenes can spike to 50/60Mbit.
     /usr/bin/plexdrive $MPOINT \
-                       -o allow_other \
-                       -v 2 &>>$LOGFILE &
+                   -o allow_other \
+                   --chunk-check-threads=10 \
+                   --chunk-load-ahead=4 \
+                   --chunk-load-threads=10 \
+                   --chunk-size=5M \
+                   --max-chunks=2000 \
+                   --refresh-interval=1m \
+                   -v 3 &>>"$LOGFILE" &
 fi
 exit
-
-## Default is with minimal options and if needed use aditional flags copy paste them above line: -v 2 &>>$LOGFILE &
-##        Note: Only lines eg options with - or -- in front
-##  --chunk-size 5M \
-##        The size of each chunk that is downloaded (units: B, K, M, G) (default "5M")
-##  --clear-chunk-age 30m0s \
-##        The maximum age of a cached chunk file (default 30m0s)
-##  --clear-chunk-interval 1m0s \
-##        The time to wait till clearing the chunk directory (default 1m0s)
-##  --clear-chunk-max-size 100G \
-##        The maximum size of the temporary chunk directory (units: B, K, M, G)
-##  -c, --config=/home/plex/.plexdrive \
-##        The path to the configuration directory (default "/home/plex/.plexdrive")
-##  -o allow_other \
-##        Fuse mount options (e.g. -fuse-options allow_other,...)
-##  --gid 1000 \
-##        Set the mounts GID (-1 = default permissions) (default -1)
-##  --refresh-interval 5m0s \
-##        The time to wait till checking for changes (default 5m0s)
-##  --speed-limit 1G \
-##        This value limits the download speed, e.g. 5M = 5MB/s per chunk (units: B, K, M, G)
-##  -t, --temp=/tmp \
-##        Path to a temporary directory to store temporary data (default "/tmp")
-##  --uid 1000 \
-##        Set the mounts UID (-1 = default permissions) (default -1)
-##  --umask value
-##        Override the default file permissions
-##  -v, --verbosity 2 \
-##        Set the log level (0 = error, 1 = warn, 2 = info, 3 = debug, 4 = trace)
