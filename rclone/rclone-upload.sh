3ee881026f9d498faa5ab559650beeaf58526c44
Ajki <flix.ajki@gmail.com>
2017-11-14 13:29:44 +0100

    Update rclone-upload.sh

--- rclone/rclone-upload.sh
+++ rclone/rclone-upload.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # RCLONE UPLOAD CRON TAB SCRIPT 
-# Type crontab -e and add line below (without #)
+# Type crontab -e and add line below (without #) and with correct path to the script
 # * * * * * /home/plex/scripts/rclone-upload.cron >/dev/null 2>&1
-# modify line 20 config file location
+# if you use custom config path add in line 20 after --log-file=$LOGFILE --config=/path/rclone.conf (config file location)
 
-if pidof -o %PPID -x "rclone-upload.cron"; then
+if pidof -o %PPID -x "$0"; then
    exit 1
 fi
 
 LOGFILE="/home/plex/logs/rclone-upload.cron"
 FROM="/storage/local/"
-TO="acdcrypt:/"
+TO="gdrivecrypt:/"
 
 # CHECK FOR FILES IN FROM FOLDER THAT ARE OLDER THAN 15 MINUTES
 if find $FROM* -type f -mmin +15 | read
   then
   echo "$(date "+%d.%m.%Y %T") RCLONE UPLOAD STARTED" | tee -a $LOGFILE
   # MOVE FILES OLDER THAN 15 MINUTES 
-  rclone move --config=/path/rclone.conf $FROM $TO --transfers=30 --checkers=30 --delete-after --min-age 15m --log-file=$LOGFILE
+  rclone move "$FROM" "$TO" --transfers=20 --checkers=20 --delete-after --min-age 15m --log-file=$LOGFILE
   echo "$(date "+%d.%m.%Y %T") RCLONE UPLOAD ENDED" | tee -a $LOGFILE
 fi
 exit
