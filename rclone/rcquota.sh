be13d350e972eca92d783c43b45b7c781270f6b2
Ajki <flix.ajki@gmail.com>
2017-11-15 11:34:46 +0100

    Create rcquota.sh

---
+++ rclone/rcquota.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## Show gdrive used quota
+Remote=$1
+LogFile=/dev/shm/rcquota.log
+
+if [ -z $1 ]; then
+    echo "Use rcquota remote:"
+fi
+    echo "Checking gdrive quota used for $Remote"
+    rclone lsd $1 -vv --dump-bodies --log-file=$LogFile >/dev/null 2>&1
+    QuotaUsed=$(grep -e 'quotaBytesUsed"' $LogFile | awk -F'[^0-9]*' '{print $2}')
+    QuotaUsed=$(($QuotaUsed / (1024*1024*1024)))
+    echo "$QuotaUsed GB"
+    rm $LogFile
+exit
